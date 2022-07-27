e77f29c235714bc49e7da35a50c0bee7241b4102
LENOVO <LENOVO@LAPTOP-7M7CPIOL>
2022-07-27 18:50:19 +0700

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+     public static void main(String args[]){
+     Jordan n1 = new Jordan();
+     Jordan n2 = new Jordan("14,070 ","1,705,116","4,548,721","47");
+       System.out.println(n2.toString());
+       
+     
+     }
+}
